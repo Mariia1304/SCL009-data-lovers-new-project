6e0acc3ef95a7ef68fbd38ba6901d78c8eab75b6
Mariia <maria.bookvoed@gmail.com>
2019-05-15 08:28:06 -0400

    cabio window.load

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 /* Manejo del DOM */
+window.addEventListener('load', function() {
+    fetch('data/worldbank/worldbank.json').then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    });
+});
